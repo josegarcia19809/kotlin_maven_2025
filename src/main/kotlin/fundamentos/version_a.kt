74744f622bc0c0431ec48c6c7839728f994ecc33
josegarcia19809 <josegarcia19809@gmail.com>
2025-09-30 14:49:33 -0600

    Fundamentos: prueba

--- src/main/kotlin/fundamentos/version_a.kt
+++ src/main/kotlin/fundamentos/version_a.kt
@@ -3,41 +3,41 @@ package org.example.fundamentos
 fun main() {
     for (noLista in 1..6) {
         println("-".repeat(50) + noLista)
-        val ventas = arrayOf(5.00, 8.00, 10.00, 15.00, 20.50, 25.00, 14.00, 10.00, 1.00)
-        var total: Double = noLista + 1.0
+        val calificaciones = arrayOf(7.5, 8.0, 9.0, 6.5, 10.0, 5.5, 8.5, 9.5, 7.0)
+        var suma = noLista + 1.0
 
-        for (venta in ventas) {
-            total += venta
+        for (calificacion in calificaciones) {
+            suma += calificacion
         }
 
-        var ventas10oMas = 0
-        for (venta in ventas) {
-            if (venta >= 10) {
-                ventas10oMas++
-            }
-        }
-
-        println("Total = $total")
-        println("Ventas mayores o iguales a 10 = $ventas10oMas")
-
-        val cigarrosFumados = arrayOf(5, 12, 8, noLista * 3, 4, 9)
-
-        var totalCigarros = 0.0
-        var aprobados = 0.0
-        var promedioCigarros = 0.0
-
-        for (cigarros in cigarrosFumados) {
-            println("Cigarros fumados al día: $cigarros")
-
-            if (cigarros < 10) {
+        var aprobados = 0
+        for (calificacion in calificaciones) {
+            if (calificacion >= 7.0) {
                 aprobados++
-                totalCigarros += cigarros
             }
         }
-        promedioCigarros = totalCigarros / aprobados
-        println("El promedio de cigarros entre los aprobados es $promedioCigarros")
+
+        println("Suma total de calificaciones = $suma")
+        println("Número de estudiantes aprobados = $aprobados")
+
+
+        val horasEstudio = arrayOf(2, 5, 8, noLista + 3, 6, 4)
+
+        var totalHoras = 0
+        var constantes = 0
+        var promedioHoras = 0.0
+
+        for (horas in horasEstudio) {
+            // println("Horas de estudio al día: $horas")
+
+            if (horas >= 5) {
+                constantes++
+                totalHoras += horas
+            }
+        }
+
+        promedioHoras = totalHoras.toDouble() / constantes
+        println("El promedio de horas de estudio entre los constantes es $promedioHoras")
+        println()
     }
 }
-
-
-
